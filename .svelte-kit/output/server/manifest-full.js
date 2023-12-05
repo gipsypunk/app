842ee7af099a448bf67f6702f7359d5dc0f5d882
gipsypunk <gipsypunk@keemail.me>
2023-12-05 18:41:06 +0100

    add

--- .svelte-kit/output/server/manifest-full.js
+++ .svelte-kit/output/server/manifest-full.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.ccc78f51.js","app":"_app/immutable/entry/app.0ac04f90.js","imports":["_app/immutable/entry/start.ccc78f51.js","_app/immutable/chunks/scheduler.63274e7e.js","_app/immutable/chunks/singletons.70ca54a9.js","_app/immutable/entry/app.0ac04f90.js","_app/immutable/chunks/scheduler.63274e7e.js","_app/immutable/chunks/index.7ac8b484.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.e34a6e68.js","app":"_app/immutable/entry/app.df5b3437.js","imports":["_app/immutable/entry/start.e34a6e68.js","_app/immutable/chunks/scheduler.63274e7e.js","_app/immutable/chunks/singletons.8d04a7f3.js","_app/immutable/entry/app.df5b3437.js","_app/immutable/chunks/scheduler.63274e7e.js","_app/immutable/chunks/index.7ac8b484.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
